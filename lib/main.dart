d9d6f0c976f530d070ee113c885da60137026c4a
zuruoke <zuruokeokafor2016@gmail.com>
2021-09-17 23:54:19 +0100

    implement backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:iwatermark/state/app_state.dart';
 import 'package:iwatermark/views/home.dart';
 import 'package:provider/provider.dart';
@@ -7,6 +8,7 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await dotenv.load();
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider<AppState>(create: (_) => AppState()),
